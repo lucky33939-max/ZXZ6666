4841fd7f406d2a2f17f2d48585a35363f817b142
lucky33939-max <lucky33939@gmail.com>
2026-04-14 19:58:21 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+DATABASE_URL = os.getenv("DATABASE_URL")
+NOWPAY_KEY = os.getenv("NOWPAY_KEY")
+
+ADMIN_ID = int(os.getenv("ADMIN_ID", "8704348652"))
+DOMAIN = os.getenv("DOMAIN")  # https://xxx.up.railway.app
