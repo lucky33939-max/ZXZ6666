d6f8bc7f891f73148462ed2f225f5a9b6db99593
lucky33939-max <lucky33939@gmail.com>
2026-04-14 19:59:07 +0700

    Create payment.py

---
+++ payment.py
@@ -0,0 +1,18 @@
+import httpx
+from config import NOWPAY_KEY
+
+API = "https://api.nowpayments.io/v1/invoice"
+
+async def create_invoice(order_id, price):
+    headers = {"x-api-key": NOWPAY_KEY}
+
+    data = {
+        "price_amount": price,
+        "price_currency": "usd",
+        "pay_currency": "usdttrc20",
+        "order_id": str(order_id)
+    }
+
+    async with httpx.AsyncClient() as client:
+        res = await client.post(API, json=data, headers=headers)
+        return res.json()["invoice_url"]
