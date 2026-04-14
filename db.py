2a0e4084d183b9ef198734ecd0f87dccb22d9177
lucky33939-max <lucky33939@gmail.com>
2026-04-14 20:20:12 +0700

    Update db.py

--- db.py
+++ db.py
@@ -1,13 +1,13 @@
 import asyncpg
 from config import DATABASE_URL
 
-db = None
+db_pool = None
 
 async def init_db():
-    global db
-    db = await asyncpg.create_pool(DATABASE_URL)
+    global db_pool
+    db_pool = await asyncpg.create_pool(DATABASE_URL)
 
-    async with db.acquire() as conn:
+    async with db_pool.acquire() as conn:
         await conn.execute("""
         CREATE TABLE IF NOT EXISTS users (
             id BIGINT PRIMARY KEY,
@@ -25,11 +25,15 @@ async def init_db():
         """)
 
 async def get_user(user_id):
-    async with db.acquire() as conn:
-        user = await conn.fetchrow("SELECT * FROM users WHERE id=$1", user_id)
+    async with db_pool.acquire() as conn:
+        user = await conn.fetchrow(
+            "SELECT * FROM users WHERE id=$1", user_id
+        )
 
         if not user:
-            await conn.execute("INSERT INTO users(id) VALUES($1)", user_id)
+            await conn.execute(
+                "INSERT INTO users(id) VALUES($1)", user_id
+            )
             return {"balance": 0}
 
         return dict(user)
