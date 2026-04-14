2ba3af39c1f3059874495ea1db43414393fbbd12
lucky33939-max <lucky33939@gmail.com>
2026-04-15 02:24:22 +0700

    Update payment.py

--- payment.py
+++ payment.py
@@ -1,18 +1,35 @@
 import httpx
 from config import NOWPAY_KEY
 
-API = "https://api.nowpayments.io/v1/invoice"
+API_URL = "https://api.nowpayments.io/v1/invoice"
 
-async def create_invoice(order_id, price):
-    headers = {"x-api-key": NOWPAY_KEY}
 
-    data = {
-        "price_amount": price,
-        "price_currency": "usd",
-        "pay_currency": "usdttrc20",
-        "order_id": str(order_id)
-    }
+async def create_invoice(order_id: int, amount: float):
+    try:
+        headers = {"x-api-key": NOWPAY_KEY}
 
-    async with httpx.AsyncClient(timeout=15) as client:
-        res = await client.post(API, json=data, headers=headers)
-        return res.json()["invoice_url"]
+        data = {
+            "price_amount": amount,
+            "price_currency": "usd",
+            "pay_currency": "usdttrc20",
+            "order_id": str(order_id),
+            "ipn_callback_url": "https://zxz666-production.up.railway.app/payment-hook"
+        }
+
+        async with httpx.AsyncClient(timeout=20) as client:
+            res = await client.post(API_URL, json=data, headers=headers)
+
+        result = res.json()
+
+        link = result.get("invoice_url")
+
+        # 💣 FIX NONE
+        if not link:
+            print("ERROR NOWPAY:", result)
+            return "❌ Payment error, try again"
+
+        return link
+
+    except Exception as e:
+        print("PAY ERROR:", e)
+        return "❌ Payment system error"
